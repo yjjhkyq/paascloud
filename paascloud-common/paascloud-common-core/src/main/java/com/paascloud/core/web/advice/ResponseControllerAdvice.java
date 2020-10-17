16eb4c6e248e595c3a09c7245b9817027afc343d
yjjhkyq <liushenyi163@163.com>
2020-10-17 21:32:12 +0800

    gateway swagger sentinel

--- paascloud-common/paascloud-common-core/src/main/java/com/paascloud/core/web/advice/ResponseControllerAdvice.java
+++ paascloud-common/paascloud-common-core/src/main/java/com/paascloud/core/web/advice/ResponseControllerAdvice.java
@@ -12,7 +12,7 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.paascloud.provider.controller")
 public class ResponseControllerAdvice implements ResponseBodyAdvice {
     @Override
     public boolean supports(MethodParameter methodParameter, Class aClass) {
